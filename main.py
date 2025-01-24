33da69164034eb7af9c55809cd13a2b79d73656e
ashwin142004 <nmashwin145@gmail.com>
2025-01-24 19:08:02 +0530

    page upadation

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from fpdf import FPDF
+import pandas as pd
 
 pdf = FPDF(orientation='P', unit='mm', format='A4')
 
-pdf.add_page()
+df = pd.read_csv('topics.csv')
 
-pdf.set_font(family = "Times", style = "B", size = 16)
-pdf.cell(w = 0, h = 12, txt = "Hello World", border = 1, ln = 1, align = 'C')
-pdf.cell(w = 0, h = 12, txt = "Hello There", border = 1, ln = 1, align = 'C')
+for index, row in df.iterrows():
+    pdf.add_page()
+    pdf.set_font(family = "Times", style = "B", size = 24)
+    pdf.set_text_color(100, 100, 100)
+    pdf.cell(w = 0, h = 12, txt = row['Topic'], border = 0, ln = 1, align = 'L')
+    pdf.line(x1 = 10, y1 = 20, x2 = 200, y2 = 20)
 
+    for pg_no in range(row['Pages'] - 1):
+        pdf.add_page()
 
 pdf.output('output.pdf')
