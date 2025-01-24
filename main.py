3c29b03938bc57ac1ce5dd901bc48216d7adadfa
ashwin142004 <nmashwin145@gmail.com>
2025-01-24 19:31:54 +0530

    Final commit

--- main.py
+++ main.py
@@ -17,10 +17,30 @@ for index, row in df.iterrows():
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w = 0, h = 12, txt = row['Topic'], border = 0, ln = 1, align = 'L')
     pdf.line(x1 = 10, y1 = 20, x2 = 200, y2 = 20)
+
+    pdf.set_draw_color(80, 80, 80)
+
+    for lines in range(30, 285, 10):
+        pdf.line(x1 = 10, y1 = lines, x2 = 200, y2 = lines)
+
+    # lines = 30
+    # while lines < 285:
+    #     pdf.line(x1 = 10, y1 = lines, x2 = 200, y2 = lines)
+    #     lines += 10
+
     footer()
     
     for pg_no in range(row['Pages'] - 1):
         pdf.add_page()
+        pdf.set_draw_color(80, 80, 80)
+        
+        for lines in range(10, 285, 10):
+            pdf.line(x1 = 10, y1 = lines, x2 = 200, y2 = lines)
+
+        # lines = 10
+        # while lines < 285:
+        #     pdf.line(x1 = 10, y1 = lines, x2 = 200, y2 = lines)
+        #     lines += 10
         footer()
 
 pdf.output('output.pdf')
